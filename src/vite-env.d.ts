32abce8ad975dfbc1c25126676cb5388e7d1afcb
nikevdp <nikevdp99@gmail.com>
2025-11-08 18:24:37 -0300

    ajutes para merge

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,4 +1,4 @@
-/// <reference types="vite/client" />
+// <reference types="vite/client" />
 
 declare module 'vite/client' {
   interface ImportMetaEnv {
@@ -11,5 +11,7 @@ declare module 'vite/client' {
     readonly VITE_COMPANY_ADDRESS_LINE1?: string
     readonly VITE_COMPANY_ADDRESS_LINE2?: string
     readonly VITE_GOOGLE_MAPS_EMBED_URL?: string
+    readonly VITE_GOOGLE_PLACES_API_KEY?: string
+    readonly VITE_GOOGLE_PLACE_ID?: string
   }
 }
