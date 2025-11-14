b7f0702c3b5b9dc2daaae2a020c9031a976ed9a9
nikevdp <nikevdp99@gmail.com>
2025-11-14 12:37:32 -0300

    ajust phone

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,10 @@ export default function App() {
 
   const handleWhatsAppClick = () => {
     if (!whatsappNumber) return;
-    window.open(`https://wa.me/${whatsappNumber}`, "_blank");
+    window.open(
+      "https://wa.me/+1170284317?text=Hola, me gustaría obtener más información",
+      "_blank"
+    );
   };
 
   return (
